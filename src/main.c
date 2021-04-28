7b88b1952b190586f49c80337c0db2afc56595cb
Nicholas Ramsay <nick@yousponsor.me>
2021-04-28 22:01:21 +1000

    adding existing files

--- src/main.c
+++ src/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
-int procedure(int x, int y, int z);
+// declare function defined in asm
+int procedure(char c1, char c2);
 
 int main() {
-    int x = 1, y = 10, z = 100, r;
-    
-    //scanf("%d%d%d", &x, &y, &z);
 
-    r = procedure(x,y,z);
-    
+    // declare input charactersand result
+    char c1, c2, r;
+
+    // get the input character from stdin
+    printf("Please enter two single digit numbers separated with a return.\n");
+    scanf("%c\n%c", &c1, &c2);
+
+    // run the procedure and place value in r
+    r = procedure(c1, c2);
+
+    // print the result
     printf("The result is %d\n", r);
+    
+    // return success code 0
     return 0;
 }
